5d62d11eac8d0ea7645c375fac12d359086041f0
Richard Remer <rich.remer@gmail.com>
2015-06-24 21:23:05 -0700

    refactor stdout writing into std module

--- atlatl/atlatl.asm
+++ atlatl/atlatl.asm
@@ -1,10 +1,13 @@
 global _start
+
 extern zstr.len
+extern std.outln
+extern std.outqwln
 
 section .text
 _start:
 	mov		rax, app_id			; zstring app id
-	call	outln
+	call	std.outln
 
 	mov		rbx, [rsp]			; argc
 	mov		rcx, 0				; arg 0
@@ -15,7 +18,7 @@ print_args:
 	mov		rax, [rsp+rcx*8+8]	; current string arg
 	push	rbx					; preserve before call
 	push	rcx					; preserve before call
-	call	outln				; print arg
+	call	std.outln			; print arg
 	pop		rcx					; recall after call
 	pop		rbx					; recall after call
 	.break:
@@ -25,75 +28,5 @@ print_args:
 	mov     rdi, 0				; success
 	syscall
 
-; out(RAX)
-; echo null-terminated string to stdout
-out:							; outln(ZSTRING)
-	push	rax					; preserve arg
-	call	zstr.len			; measure string
-
-	pop		rsi					; message
-	mov		rdx, rax			; length returned
-	mov		rax, 1				; sys_write
-	mov		rdi, 1				; stdout
-	syscall
-
-	ret
-
-; outln(RAX)
-; echo null-terminated string to stdout, followed by newline
-outln:							; outln(ZSTRING)
-	call	out					; echo string
-
-	mov		rax, 1				; sys_write
-	mov		rdi, 1				; stdout
-	mov		rsi, .endln			; message
-	mov		rdx, 0x1			; length
-	syscall
-
-	ret
-	.endln:	db	0xa
-
-; qwout(RAX)
-; echo 64 bit value to stdout
-qwout:							; qwout(QWORD)
-
-	mov		rcx, 0x10			; number of characters
-	.char:
-	mov		rbx, rax			; make copy
-	and		rbx, 0xf 			; mask low bits
-	mov		rsi, hexits			; read buffer
-	mov		rdi, qwbuf			; write buffer
-	mov		dl, [rbx+rsi]		; lookup hexit
-	mov		[rcx+rdi-1], dl		; move hexit into buffer
-	shr		rax, 4				; consume bits
-	dec		rcx					; decrement character counter
-	jnz		.char				; loop
-
-	mov		rax, 1				; sys_write
-	mov		rdi, 1				; stdout
-	mov		rsi, qwbuf			; message
-	mov		rdx, 0x10			; number of characters
-	syscall
-
-	ret
-
-; qwoutln(RAX)
-; echo 64 bit value to stdout, followed by newline
-qwoutln:						; qwoutln(QWORD)
-	call	qwout				; echo value
-
-	mov		byte[qwbuf], 0xa	; put newline in buffer
-	mov		rax, 1				; sys_write
-	mov		rdi, 1				; stdout
-	mov		rsi, qwbuf			; message
-	mov		rdx, 0x1			; number of characters
-	syscall
-
-	ret
-
 section .data
-hexits:		db		"0123456789ABCDEF"
 app_id:		db		"atlatl v0.0.1α", 0x0
-
-section .bss
-qwbuf:	resb	16
---
+++ atlatl/std.asm
@@ -0,0 +1,77 @@
+global std.out
+global std.outln
+global std.outqw
+global std.outqwln
+
+extern zstr.len
+
+section .text
+
+; std.out(RAX)
+; print null-terminated string to stdout
+std.out:
+	push	rax			; preserve arg
+	call	zstr.len	; measure string
+
+	pop		rsi			; message
+	mov		rdx, rax	; length returned
+	mov		rax, 1		; sys_write
+	mov		rdi, 1		; stdout
+	syscall
+
+	ret
+
+; std.outln(RAX)
+; print null-terminated string to stdout, followed by newline
+std.outln:
+	call	std.out		; echo string
+
+	mov		rax, 1		; sys_write
+	mov		rdi, 1		; stdout
+	mov		rsi, .endln	; message
+	mov		rdx, 1		; length
+	syscall
+
+	ret
+	.endln:	db	0xa
+
+; std.outqw(RAX)
+; print QWord in hex to stdout
+std.outqw:
+	mov		rcx, 16			; number of characters
+	.char:
+	mov		rbx, rax		; make copy
+	and		rbx, 0xf		; mask low bits
+	mov		rsi, hexits		; read buffer
+	lea		rdi, [rsp-16]	; write buffer
+	mov		dl, [rbx+rsi]	; lookup hexit
+	mov		[rcx+rdi-1], dl	; move hexit into buffer
+	shr		rax, 4			; consume bits
+	dec		rcx;			; decrement character counter
+	jnz		.char			; loop to next character
+
+	mov		rax, 1			; sys_write
+	mov		rdi, 1			; stdout
+	lea		rsi, [rsp-16]	; message
+	mov		rdx, 16			; number of characters
+	syscall
+
+	ret
+
+; std.outqwln(RAX)
+; print QWord in hex to stdout, followed by newline
+std.outqwln:
+	call	std.outqw		; echo value
+
+	mov		rax, 1			; sys_write
+	mov		rdi, 1			; stdout
+	mov		rsi, .endln		; message
+	mov		rdx, 1			; number of characters
+	syscall
+
+	ret
+	.endln:	db	0xa
+
+section .data
+hexits:	db	"0123456789ABCDEF"
+
