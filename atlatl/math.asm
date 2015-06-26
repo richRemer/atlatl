be2dc68e75cfca7f4bb520ae11c87d75430e20c2
Richard Remer <rich.remer@gmail.com>
2015-06-26 15:07:23 -0700

    math.log2

---
+++ atlatl/math.asm
@@ -0,0 +1,24 @@
+global math.log2
+
+extern sys.error
+
+%define EUNDEF  0xf0
+
+; math.log2(RAX) => RAX
+; calculate base 2 logarithm of an integer
+math.log2:              ; math.log2(val)
+    test    rax, rax    ; validate argument
+    jnz     .valid      ; arg is good if not 0
+    mov     rax, EUNDEF ; log2(0) is undefined
+    call    sys.error
+
+    .valid:
+    mov     rcx, -1     ; begin count
+
+    .shift:
+    inc     rcx         ; count shift
+    shr     rax, 1      ; perform shift
+    jnz     .shift      ; stop at 0
+
+    mov     rax, rcx    ; return number of shifts
+    ret
