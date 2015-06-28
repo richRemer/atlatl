e1f0fa6edd068e6b33b628c7809260f866bad4de
Richard Remer <rich.remer@gmail.com>
2015-06-27 22:18:39 -0700

    implemented mem module with hilariously bad free implementation

--- atlatl/atlatl.asm
+++ atlatl/atlatl.asm
@@ -1,16 +1,11 @@
 global _start
 
-extern mem.create_index
 extern std.outln
 extern zstr.eachq
 extern sys.exit
 
 section .text
 _start:
-    ;; initialize dynamic memory; must be called before alloc and
-    ;; friends
-    call    mem.create_index
-
     mov     rax, app_id     ; zstring app id
     call    std.outln
 
--- atlatl/mem.asm
+++ atlatl/mem.asm
@@ -9,62 +9,56 @@ extern sys.error
 %define ENOMEM  0x55
 %define ENOIMP  0x11
 
-struc Entry
-    .capacity:  resq    1
-    .index:     resq    1
-    .size:
-endstruc
-
-struc Index
-    .entries:   resb    Entry.size*64
-    .size:
-endstruc
-
 section .text
-
-; mem.create_index()
-; create the memory index
-mem.create_index:
-    push    rax             ; preserve
-    push    rdi             ; preserve
-
-    mov     rax, 12         ; sys_brk
-    mov     rdi, 0          ; get current brk
-    syscall
-
-    push    rax             ; current brk
-    pop     qword[mem.index]; set global index
-    mov     rdi, rax        ; current brk
-    add     rdi, Index.size ; add space for index
-    mov     rax, 12         ; sys_brk
-    syscall
-
-    cmp     rax, 0          ; result should be new brk
-    jge     .exit           ; all good
-
-    mov     rax, ENOMEM     ; out of memory error
-    call    sys.error
-
-    .exit:
-    pop     rdi             ; restore
-    pop     rax             ; restore
-    ret
 
 ; mem.alloc(RAX) => RAX
 ; allocate memory
-mem.alloc:                  ; mem.alloc(size) => ptr
+mem.alloc:                                  ; mem.alloc(size) => ptr
+    test    rax, rax                        ; check allocation size
+    jnz     .non_empty                      ; non-empty allocation
+    inc     qword[mem.zeros]                ; empty allocation ref
+    mov     rax, [mem.zeros]                ; count
+    ret
+
+    .non_empty:
+    push    rbx                             ; preserve
+    push    rdi                             ; preserve
+
+    mov     rbx, rax                        ; save requested size
+
+    mov     rax, 12                         ; sys_brk
+    mov     rdi, 0                          ; get current brk
+    syscall
+
+    mov     rdi, rax                        ; current brk
+    add     rdi, rbx                        ; + requested bytes
+    mov     rax, 12                         ; sys_brk
+    syscall
+
+    cmp     rax, 0                          ; result should be new brk
+    jge     .exit                           ; all good
+
+    mov     rax, ENOMEM                     ; out of memory
+    call    sys.error
+
+    .exit:
+    sub     rax, rbx                        ; ptr to new memory
+    pop     rdi                             ; restore
+    pop     rbx                             ; restore
+    ret
 
 ; mem.free(RAX)
-; free memory
-mem.free:                   ; mem.free(ptr)
+; free memory (ha!)
+mem.free:                                   ; mem.free(ptr)
+    ret
 
 ; mem.realloc(RAX, RBX) => RAX
 ; reallocate memory
-mem.realloc:                ; mem.free(ptr, size) => size
-
-; unimplemented
-    mov     rax, ENOIMP     ; not implemented
-    call    sys.error
+mem.realloc:                                ; mem.free(ptr, size) => size
+    mov     rax, rbx                        ; size
+    call    mem.alloc                       ; just get new memory
+    ret
 
 section .bss
 mem.index:  resq    1
+mem.zeros:  resq    1
